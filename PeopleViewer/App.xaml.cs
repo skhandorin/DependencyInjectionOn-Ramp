2f10a1210f85b66b8e457b683b0103ca4c5499fe
SKhandorin <hsatig@mail.ru>
2018-03-15 00:22:47 +0500

    Add project files.

---
+++ PeopleViewer/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using PersonRepository.Interface;
+using PersonRepository.Service;
+using PeopleViewer.Presentation;
+using PersonRepository.CSV;
+using PersonRepository.SQL;
+
+namespace PeopleViewer
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ComposeObjects();
+            Application.Current.MainWindow.Title = "Loose Coupling - People Viewer";
+            Application.Current.MainWindow.Show();
+        }
+
+        private static void ComposeObjects()
+        {
+            IPersonRepository repository = new SQLRepository(); ;
+            var viewModel = new PeopleViewerViewModel(repository);
+            Application.Current.MainWindow = new PeopleViewerWindow(viewModel);
+        }
+    }
+}
---
+++ PersonRepository.Interface/IPersonRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using PeopleViewer.SharedObjects;
+
+namespace PersonRepository.Interface
+{
+    public interface IPersonRepository
+    {
+        IEnumerable<Person> GetPeople();
+
+        Person GetPerson(string lastName);
+
+        void AddPerson(Person newPerson);
+
+        void UpdatePeople(IEnumerable<Person> updatedPeople);
+
+        void UpdatePerson(string lastName, Person updatedPerson);
+
+        void DeletePerson(string lastName);
+
+    }
+}
