f77125c45ca3121c0e5da4681ce3a8d2a9483eca
SKhandorin <hsatig@mail.ru>
2018-03-16 01:26:27 +0500

    4.3

---
+++ PeopleViewer.Presentation.Test/PeopleViewerViewModelTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using PersonRepository.Interface;
+using System.Collections.Generic;
+using PeopleViewer.SharedObjects;
+using Moq;
+
+namespace PeopleViewer.Presentation.Test
+{
+    [TestClass]
+    public class PeopleViewerViewModelTest
+    {
+        IPersonRepository _repository;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var people = new List<Person>()
+            {
+                new Person() { FirstName = "John", LastName = "Smith", Rating = 7, StartDate = DateTime.Parse("01.10.2000") },
+                new Person() { FirstName = "Mary", LastName = "Thomas", Rating = 9, StartDate = DateTime.Parse("23.07.1971") }
+            };
+
+            var repoMock = new Mock<IPersonRepository>();
+            repoMock.Setup(r => r.GetPeople()).Returns(people);
+            _repository = repoMock.Object;
+        }
+
+        [TestMethod]
+        public void People_OnRefreshCommand_Populated()
+        {
+            // Arrange
+            var vm = new PeopleViewerViewModel(_repository);
+
+            // Act
+            vm.RefreshPeopleCommand.Execute(null);
+
+            // Assert
+            Assert.IsNotNull(vm.People);
+            Assert.AreEqual(2, vm.People.Count());
+        }
+
+        [TestMethod]
+        public void People_OnClearCommand_Empty()
+        {
+            // Arrange
+            var vm = new PeopleViewerViewModel(_repository);
+            vm.RefreshPeopleCommand.Execute(null);
+            Assert.AreEqual(2, vm.People.Count(), "Invalid Arrangement");
+
+            // Act
+            vm.ClearPeopleCommand.Execute(null);
+
+            // Assert
+            Assert.AreEqual(0, vm.People.Count());
+        }
+    }
+}
---
+++ PeopleViewer.Presentation.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PeopleViewer.Presentation.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("PeopleViewer.Presentation.Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b58f5604-0281-410c-af7a-491b6c082204")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
